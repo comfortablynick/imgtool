9ca95c60ed66766dfe3f388cd475219700f36d67
Nick Murphy <comfortablynick@gmail.com>
2019-09-11 12:14:28 -0500

    Add RGB -> CMYK and RGB -> Grayscale

--- imgtool.py
+++ imgtool.py
@@ -4,9 +4,27 @@
 import pyvips
 import logging
 from dataclasses import dataclass
+from typing import Tuple, NamedTuple, Any
 
 logging.basicConfig(level=logging.INFO)
 LOG = logging.getLogger(__name__)
+
+
+class RGB(NamedTuple):
+    """Red, green, blue colors."""
+
+    r: int
+    g: int
+    b: int
+
+
+class CMYK(NamedTuple):
+    """Cyan, magenta, yellow, black colors."""
+
+    c: int
+    m: int
+    y: int
+    k: int
 
 
 @dataclass
@@ -14,10 +32,11 @@ class TextWatermark:
     """Specify settings of text watermark."""
 
     text: str
+    fg_color: RGB = RGB(0, 0, 0)
     x_margin: int = 25
     y_margin: int = 25
     font: str = "sans 12"
-    rotate_degrees: int = 0
+    rotate: int = 0
     opacity: float = 0.7
     replicate: bool = False
 
@@ -26,7 +45,7 @@ class TextWatermark:
         text = pyvips.Image.text(
             self.text, width=im.width, dpi=300, font=f"{self.font}"
         )
-        text = text.rotate(self.rotate_degrees)
+        text = text.rotate(self.rotate)
         text = (text * self.opacity).cast("uchar")
         text = text.embed(
             self.x_margin,
@@ -41,24 +60,22 @@ class TextWatermark:
             )
             text = text.crop(0, 0, im.width, im.height)
 
-        # TODO: convert RGB to CMYK/mono on the fly
-        colors = {"cmyk": [0, 0, 0, 0], "rgb": [255, 255, 255], "mono": 255}
-
         # we want to blend into the visible part of the image and leave any alpha
         # channels untouched ... we need to split im into two parts
         # guess how many bands from the start of im contain visible colour information
         if im.bands >= 4 and im.interpretation == pyvips.Interpretation.CMYK:
             # cmyk image
             n_visible_bands = 4
-            text_color = colors["cmyk"]
+            text_color: Any = list(rgb_to_cmyk(self.fg_color))
         elif im.bands < 4:
             # rgb image
             n_visible_bands = 3
-            text_color = colors["rgb"]
+            text_color = list(self.fg_color)
         else:
             # mono image
             n_visible_bands = 1
-            text_color = colors["mono"]
+            text_color = rgb_to_grayscale(self.fg_color)
+        LOG.info("Watermark fg_color: %s (original: %s)", text_color, self.fg_color)
 
         # split into image and alpha
         if im.bands > n_visible_bands:
@@ -76,15 +93,41 @@ class TextWatermark:
         return im
 
 
+def rgb_to_cmyk(rgb: RGB) -> CMYK:
+    """Convert rgb color to cmyk color."""
+    float2int = lambda n: int(round(n))
+
+    # Convert from 0..255 to 0..1
+    LOG.info("RBG value: %s", rgb)
+    rgb_adj = (rgb.r / 255, rgb.g / 255, rgb.b / 255)
+    k = float2int(1 - max(rgb_adj))
+    c = float2int((1 - rgb_adj[0] - k) / (1 - k))
+    m = float2int((1 - rgb_adj[1] - k) / (1 - k))
+    y = float2int((1 - rgb_adj[2] - k) / (1 - k))
+    cmyk = CMYK(c, m, y, k)
+    LOG.info("CMYK value: %s", cmyk)
+    return cmyk
+
+
+def rgb_to_grayscale(rgb: RGB) -> int:
+    """Convert rgb to grayscale with weighted method."""
+    LOG.info("RGB value: %s", rgb)
+    gray = int(round((0.299 * rgb.r) + (0.587 * rgb.g) + (0.114 * rgb.b)))
+    LOG.info("Grayscale value: %d", gray)
+    return gray
+
+
 def main():
     """Entry point."""
     in_filename = "./test/chin_class.jpg"
     out_filename = "./test/chin_class_edited.jpg"
     watermark_text = "© 2019 Nick Murphy | murphpix.com"
+    LOG.info("Adding watermark '%s'", watermark_text)
 
     im = pyvips.Image.new_from_file(in_filename, access=pyvips.Access.SEQUENTIAL)
     watermark = TextWatermark(watermark_text)
-    watermark.font = "sans 12"
+    watermark.font = "Source Sans Pro 16"
+    watermark.rotate = -90
     watermark.opacity = 1
 
     im = watermark.add_to_image(im)
